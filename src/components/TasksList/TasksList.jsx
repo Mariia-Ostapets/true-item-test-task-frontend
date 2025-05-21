0f07084527034039fa76de1ad0227e7a77c2df63
Mariia_Ostapets <mariyaostapets@gmail.com>
2025-05-21 17:41:28 +0300

    Fix loading tasks

--- src/components/TasksList/TasksList.jsx
+++ src/components/TasksList/TasksList.jsx
@@ -1,12 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux';
 import css from './TasksList.module.css';
-import { selectTasks } from '../../redux/tasks/selectors';
+import { selectIsLoading, selectTasks } from '../../redux/tasks/selectors';
 import Task from '../Task/Task';
+import Loader from '../ui/Loader/Loader';
 import { useEffect } from 'react';
 import { fetchTasks } from '../../redux/tasks/operations';
 
 export default function TasksList() {
   const tasks = useSelector(selectTasks);
+  const isLoading = useSelector(selectIsLoading);
 
   const dispatch = useDispatch();
 
@@ -15,10 +17,11 @@ export default function TasksList() {
   }, [dispatch]);
 
   return (
-    <section>
-      {tasks.length === 0 ? (
+    <section className={css.wrapper}>
+      {tasks.length === 0 && !isLoading && (
         <p className={css.noTasks}>No tasks added yet</p>
-      ) : (
+      )}
+      {tasks.length > 0 && (
         <ul className={css.list}>
           {tasks.map(task => (
             <li className={css.listItem} key={task._id}>
